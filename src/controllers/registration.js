bd259a4eee17f16d8f218161994b6c7e587f8ee6
Vadim Shkolenko <vadimand1998@gmail.com>
2021-03-21 17:48:47 +0300

    реализовал подтверждение email и генерацию токена

--- src/controllers/registration.js
+++ src/controllers/registration.js
@@ -1,5 +1,6 @@
 const {v4: uuid} = require('uuid');
 const User = require('../models/User');
+const {sendMail} = require('../libs/sendMail');
 
 module.exports.register = async (ctx) => {
   const {email, password, username} = ctx.request.body;
@@ -9,5 +10,28 @@ module.exports.register = async (ctx) => {
   await user.setPassword(password);
   await user.save();
 
+  await sendMail({
+    template: 'confirmation',
+    locals: {token: verificationToken},
+    to: email,
+    subject: 'Подтвердите почту',
+  });
+
+  ctx.status = 200;
   ctx.body = {status: 'ok'};
 }
+
+module.exports.confirm = async (ctx) => {
+  const {verificationToken} = ctx.request.body;
+
+  const user = await User.findOneAndUpdate({verificationToken}, {$unset: {verificationToken}});
+
+  if (user) {
+    const token = await ctx.login(user)
+
+    ctx.body = {token}
+  } else {
+    ctx.status = 400;
+    ctx.body = {error: 'Ссылка подтверждения недействительна или устарела'};
+  }
+};
